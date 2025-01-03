ce3a3d33f3f4c34c98f5b32c57fb9d96ea536926
romano <romanong321@gmail.com>
2025-01-03 11:51:49 -0400

    mas rutas armas

--- src/equipo-armamento-ms/equipo-armamento-ms.controller.ts
+++ src/equipo-armamento-ms/equipo-armamento-ms.controller.ts
@@ -1,9 +1,9 @@
-import { BadRequestException, Body, Controller, Get, Inject, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, Inject, InternalServerErrorException, Param, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
 
 import { RABBITMQ_SERVICE } from 'src/config';
-import { CreateArmaDto } from './dto';
+import { CreateArmaDto, UpdateArmaDto } from './dto';
 
 @Controller('articulo')
 export class EquipoArmamentoMsController {
@@ -54,4 +54,60 @@ export class EquipoArmamentoMsController {
       throw new RpcException(error);
     }
   }
+  @Get("arma/:id")
+  async findOneArma(@Param('id') id: string) {
+    try {
+      const arma = await firstValueFrom(
+        this.client.send('get.articulo.arma.id', { id }) //TODO manejo de errores: si no ponemos objeto lo encuentra
+      )
+      return arma;
+    } catch (error) {
+      console.log(error);
+      if (error.status === 400) {
+        throw new BadRequestException(error.message);
+      } else {
+        throw new InternalServerErrorException('Error desconocido al actualizar el arma.');
+      }
+      // throw new RpcException(error);
+    }
+  }
+  @Patch("arma/:id")
+  async updateArma(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateArma: UpdateArmaDto
+  ) {
+    try {
+      const arma = await firstValueFrom(
+        this.client.send('update.articulo.arma', { id, ...updateArma })
+      )
+      return arma;
+    } catch (error) {
+      console.log(error);
+      if (error.status === 400) {
+        throw new BadRequestException(error.message);
+      } else {
+        throw new InternalServerErrorException('Error desconocido al actualizar el arma.');
+      }
+      // throw new RpcException(error);
+    }
+  }
+
+  @Delete("arma/:id")
+  async removeArma(@Param('id', ParseUUIDPipe) id: string) {
+    try {
+      const arma = await firstValueFrom(
+        this.client.send('delete.articulo.arma', { id }) //TODO manejo de error
+      )
+      return arma;
+    } catch (error) {
+      console.log(error);
+      if (error.status === 400) {
+        throw new BadRequestException(error.message);
+      } else {
+        throw new InternalServerErrorException('Error desconocido al eliminar el arma.');
+      }
+    }
+  }
+
+  // EQUIPOS  
 }
